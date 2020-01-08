07f1acba88bba000a627ad3ea40acd5a3d5d33df
Phil Millington-Hore <phil.millington-hore@isams.co.uk>
2020-01-08 16:32:35 +0000

    feature(odic-example-app): vs2017 - initial add

---
+++ VS2017/iSAMS-OpenIdConnect/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iSAMS_OpenIdConnect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View("Claims");
+        }
+
+        public IActionResult Logout()
+        {
+            return new SignOutResult(new[]
+                {OpenIdConnectDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme});
+        }
+
+        [Authorize]
+        public async Task<ActionResult> MakeApiCall()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                httpClient.BaseAddress = new Uri("https://developerdemo.isams.cloud");
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var httpRequest = new HttpRequestMessage(HttpMethod.Get, "api/estates/buildings");
+
+                var response = await httpClient.SendAsync(httpRequest, CancellationToken.None);
+                var result = await response.Content.ReadAsStringAsync();
+
+                return View("MakeApiCall", result);
+            }
+        }
+    }
+}
---
+++ VS2017/iSAMS-OpenIdConnect/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace iSAMS_OpenIdConnect
+{
+    public class Startup
+    {
+        private const string Authority = "https://developerdemo.isams.cloud/auth";
+        private const string ClientId = "isams.oidc.demo";
+
+        private const string ClientSecret = "40019D7D-7641-4A54-9F8C-9FF08AEC0614";
+
+        // Set required response type(s), see https://developer-beta.isams.com/docs/getting-started-single-sign-on#section-single-sign-on
+        private const string ResponseType = "code id_token token";
+
+        // Add required scopes, see https://developer-beta.isams.com/docs/scopes
+        private const string ScopesCsv = "openid,email,profile";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // OpenIDConnect authentication event handling.
+            services.AddTransient<AuthenticationEvents, AuthenticationEvents>();
+
+            // Allow claims to pass through without being changed by Microsoft.
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            // Add cookie authentication under the default authentication scheme.
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(options =>
+                {
+                    options.Authority = Authority;
+                    options.ClientId = ClientId;
+                    options.ClientSecret = ClientSecret;
+                    options.RequireHttpsMetadata = false;
+                    options.ResponseType = ResponseType;
+
+                    // Add required scopes, see https://developer-beta.isams.com/docs/scopes
+                    options.Scope.Clear();
+                    foreach (var scope in ScopesCsv.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        options.Scope.Add(scope);
+                    }
+
+                    // Save tokens for future use.
+                    options.SaveTokens = true;
+
+                    // Have the authentication provider gather claims for us
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.EventsType = typeof(AuthenticationEvents);
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Standard configuration.
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+
+            // Authentication MUST be added before MVC or the user context will not be created
+            // before the request reaches the application.
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
+        }
+    }
+}
